14690222ac87a81cfbc047d78ec0bbf380c4fc82
arti0090 <arti0090@gmail.com>
2021-09-05 19:29:54 +0200

    Navbar mostly

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        build : "Site is still in build phase.",
+        message: 'Hello Vue!',
+        placeholder: `
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it to make a type
+            specimen book. It has survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged. It was popularised in
+            the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus PageMaker
+            including versions of Lorem Ipsum.
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non lectus consequat, tincidunt velit in, suscipit ante. Sed aliquam felis in nisi tempor, vitae efficitur augue convallis. Curabitur non justo tortor. Nunc quis metus vitae leo rutrum iaculis. Phasellus mattis lorem et suscipit pharetra. Integer mollis porttitor quam vel fermentum. Pellentesque aliquam interdum tempor.
+            </p><p>
+            Vivamus porta ultricies est, in varius ex posuere quis. In ultrices eros lectus, non elementum justo cursus in. Morbi eget feugiat metus, sed porta neque. Integer pharetra nibh sed dignissim luctus. Aliquam pharetra finibus dui. Etiam nec neque eget leo pharetra sollicitudin. Pellentesque ac facilisis est. Aliquam vel erat non elit congue rhoncus nec ut sem. Nam quis orci sit amet erat sodales auctor a id erat.
+            </p><p>
+            Suspendisse cursus tincidunt risus, ac sodales dui auctor sit amet. Nullam aliquam, nisi porta iaculis lobortis, elit ipsum pharetra dui, sed condimentum nisl mi sed nisl. Quisque dictum maximus velit, in faucibus dui ultricies vel. Cras in ipsum bibendum, ullamcorper ex ac, tincidunt lectus. Nunc quis mauris velit. In ut dui vel sem fermentum bibendum. Sed congue justo non viverra porttitor.
+            </p><p>
+            Sed eros massa, cursus vel sapien quis, ultrices rutrum dui. In et felis ut eros blandit lobortis. Fusce et dignissim ligula, sed pharetra mi. Proin eleifend consequat urna, in sodales felis vulputate ut. Praesent nunc lacus, bibendum in odio eget, lacinia posuere nulla. Cras lobortis et enim nec luctus. Nam eu euismod leo. Donec finibus fringilla purus. Praesent non justo arcu. Phasellus a varius leo. Morbi luctus ac turpis non vestibulum. Aliquam sit amet turpis metus. Cras sagittis tempus massa, at consequat dui aliquam ac. Donec interdum nulla id neque gravida cursus.
+            </p><p>
+            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed vitae nunc id ex euismod suscipit. Morbi ut odio lorem. Curabitur non viverra sem. Quisque sed libero ligula. Nulla in dui lectus. Quisque sit amet lorem sed ipsum vulputate feugiat nec bibendum massa. Nunc posuere eget ex ut fermentum. Quisque commodo libero nisi, quis varius neque tristique nec. Cras non sapien a nunc ultrices consequat. Pellentesque faucibus justo vel diam dignissim dictum.
+            </p>
+            `
+    }
+})
